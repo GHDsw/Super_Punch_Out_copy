49a54330e9831e3a270f71e60b81954ee1fc17f9
GHDsw <hsw29371@gmail.com>
2025-11-10 02:40:53 +0900

    배경 애니메이션 구현

--- grass.py
+++ grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import game_framework
 
 import game_world
 
@@ -8,17 +9,36 @@ sprite_size = {'ring1': [[0, 0], [431, 175]],
                'ring4': [[0, 768], [431, 944]],
                }
 
+TIME_PER_ACTION = 1
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 4
+
+image = None
+
 class Grass:
     def __init__(self):
 
         self.image = load_image('./image/Boxing_Rings.png')
+        self.frame = 0
 
     def update(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
         pass
 
     def draw(self):
-        self.image.draw(400, 30)
-        self.image.draw(1200, 30)
+        sx, sy = sprite_size['ring1'][0]
+        ex, ey = sprite_size['ring1'][1]
+
+        img_h = self.image.h  # 이미지 전체 높이
+        gap = 80  # 프레임 간격
+        clip_x = sx
+        clip_y = img_h - ey - 1  # top-based y -> bottom-based y 변환
+        clip_w = ex - sx + 1
+        clip_h = ey - sy + 1
+        #4프레임, 80 차이
+        self.image.clip_draw(clip_x,
+                        clip_y - (clip_h * (int(self.frame) % 4) + gap * (int(self.frame) % 4)),
+                        clip_w, clip_h, 400, 300)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
