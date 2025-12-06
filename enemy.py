f5565e088a678c018bb96c0ec28cf02f26807b62
GHDsw <hsw29371@gmail.com>
2025-12-06 18:31:18 +0900

    BT 수정

--- enemy.py
+++ enemy.py
@@ -30,23 +30,14 @@ GIMMIK_PER_ACTION = 6
 OUT_PER_ACTION = 12
 
 sprite_size = {
-    #임시 이미지
-    'Idle': [[0, 0], [69, 168]],
-    'Move': [[305, 0], [366, 168]], 'Move2': [[366, 0], [453, 168]],
-    'Def': [[0, 169], [83, 345]],
-    'Hit': [[60, 1570], [146, 1762]], 'Hit': [[147, 1570], [225, 1762]],
-    'Stun': [[0, 1234], [ 73, 1392]],
-    'Atk_up': [[84, 365], [153, 530]],
-    'Atk_down': [[153, 365], [222, 530]],
-    
+    #'상태_스탠스_프레임': [[시작x, 시작y], [끝x, 끝y]]
+    'Idle_-1_1': [[0, 0], [69, 168]], 'Idle_-1_2': [[70, 0], [132, 168]], 'Idle_1_1': [[133, 0], [212, 168]], 'Idle_1_2': [[134, 0], [277, 168]], 'Move_1': [[315, 0], [383, 168]], 'Move_2': [[384, 0],[453, 168]],
 
-    '1': [[0, 0], [69, 168]], '2': [[70, 0], [132, 168]], '3': [[133, 0], [212, 168]], '4': [[134, 0], [277, 168]], '5': [[315, 0], [383, 168]], '6': [[384, 0],[453, 168]],
+    'Def_1': [[0, 169], [70, 345]], 'Def_-1': [[71, 169], [135, 345]],
 
-    '1': [[0, 169], [70, 345]], '2': [[71, 169], [135, 345]],
+    'Atk_1_1': [[0, 170], [75, 527]], 'Atk_1_2': [[76, 170], [146, 527]],
 
-    '1': [[0, 170], [75, 527]], '2': [[76, 170], [146, 527]],
-
-    '1': [[0, 528], [71, 702]], '2': [[72, 528], [154, 702]], '3': [[155, 528], [253, 702]],
+    'Atk_-1_1': [[0, 528], [71, 702]], 'Atk_-1_2': [[72, 528], [154, 702]], 'Atk_-1_3': [[155, 528], [253, 702]],
 
     '1': [[0, 703], [58, 866]], '2': [[59, 703], [145, 866]], '3': [[146, 703], [252, 866]],
 
@@ -54,13 +45,13 @@ sprite_size = {
 
     '1': [[0, 1044], [73, 1236]], '2': [[74, 1044], [159, 1236]],
 
-    '1': [[0, 1045], [73, 1393]], '2': [[0, 1045], [152, 1393]],
+    'Stun_1': [[0, 1045], [73, 1393]], 'Stun_2': [[0, 1045], [152, 1393]],
 
-    '1': [[0, 1394], [65, 1569]], '1': [[66, 1394], [137, 1569]], '1': [[138, 1394], [231, 1569]], '1': [[232, 1394], [311, 1569]], '1': [[312, 1394], [402, 1569]], '1': [[403, 1394], [467, 1569]],
+    '1': [[0, 1394], [65, 1569]], '2': [[66, 1394], [137, 1569]], '3': [[138, 1394], [231, 1569]], '4': [[232, 1394], [311, 1569]], '5': [[312, 1394], [402, 1569]], '6': [[403, 1394], [467, 1569]],
 
-    '1': [[0, 1570], [59, 1762]], '1': [[60, 1570], [146, 1762]], '1': [[147, 1570], [225, 1762]], '1': [[226, 1570], [330, 1762]], '1': [[331, 1570], [405, 1762]], '1': [[406, 1570], [501, 1762]],
+    'Hit_1': [[0, 1570], [59, 1762]], 'Hit_-1': [[60, 1570], [146, 1762]], '3': [[147, 1570], [225, 1762]], '4': [[226, 1570], [330, 1762]], '5': [[331, 1570], [405, 1762]], '6': [[406, 1570], [501, 1762]],
 
-    '1': [[0, 1763], [102, 1944]], '1': [[103, 1763], [203, 1944]], '1': [[204, 1763], [306, 1944]], '1': [[307, 1763], [384, 1944]], '1': [[385, 1763], [450, 1944]],
+    '1': [[0, 1763], [102, 1944]], '2': [[103, 1763], [203, 1944]], '3': [[204, 1763], [306, 1944]], '4': [[307, 1763], [384, 1944]], '5': [[385, 1763], [450, 1944]],
 }
 
 class Enemy:
@@ -70,9 +61,9 @@ class Enemy:
     def __init__(self):
 
         self.x, self.y = 400, 300
-        self.dir = 0
+        self.dir = 0 # 1: 오른쪽, -1:왼쪽
         self.hp = 10
-        self.stance = -1  #1: 상단, -1: 하단
+        self.stance = -1  # 1: 상단, -1: 하단
         self.state = 'Idle'
 
         self.image = load_image('./image/Gabby_Jay.png')
@@ -96,6 +87,8 @@ class Enemy:
 
 
     def draw(self):
+
+
         self.image.clip_composite_draw(self.clip_x, self.clip_y, self.clip_w, self.clip_h,
                                        0, 'h',
                                        self.x, self.y + 120,
@@ -110,74 +103,65 @@ class Enemy:
         if group == 'boy:enemy':
             self.hp -= 10
 
-    def Idle(self):
-        self.state = 'Idle'
-        sx, sy = sprite_size['Idle'][0]
-        ex, ey = sprite_size['Idle'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
+    def stance_dir_set(self):
+        self.stance = random.choice([1, -1])
+        self.dir = random.choice([1, -1])
         return BehaviorTree.SUCCESS
 
-    def stance(self):
-        #스탠스 결정
-        self.stance = random.choice([1, -1])
+    def Idle(self):
+        self.state = 'Idle'
         return BehaviorTree.SUCCESS
 
     def Move(self):
         self.state = 'Move'
-        sx, sy = sprite_size['Move'][0]
-        ex, ey = sprite_size['Move'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
-        return BehaviorTree.SUCCESS
-
-    def direction(self):
-        #이동 방향 결정
-        self.dir = random.choice([1, -1])
         return BehaviorTree.SUCCESS
 
     def Attack(self):
         self.state = 'Atk'
-        sx, sy = sprite_size['Atk'][0]
-        ex, ey = sprite_size['Atk'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
+        return BehaviorTree.SUCCESS
+
+    def Stun(self):
+        #기절 상태
+        self.state = 'Stun'
         return BehaviorTree.SUCCESS
 
     def Defend(self):
         self.state = 'Def'
-        sx, sy = sprite_size['Def'][0]
-        ex, ey = sprite_size['Def'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
         return BehaviorTree.SUCCESS
 
-    def stance_check(self):
-        #boy의 dir 확인
-        if common.boy.dir == 1 and self.stance == 1:
-            return BehaviorTree.SUCCESS
-        elif common.boy.dir == -1 and self.stance == -1:
+    def Hit(self):
+        self.state = 'Hit'
+        return BehaviorTree.SUCCESS
+
+    def move_chk(self):
+        #이동 중 피격
+        if self.state == 'Move':
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
 
-    def Hit(self):
-        self.state = 'Hit'
-        sx, sy = sprite_size['Hit'][0]
-        ex, ey = sprite_size['Hit'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
-        return BehaviorTree.SUCCESS
-
-    def Atk_ing(self):
+    def atk_ing_chk(self):
         #공격 도중 피격
         if self.state == 'Atk':
             return BehaviorTree.SUCCESS
         else:
             return BehaviorTree.FAIL
 
-    def stun(self):
-        #기절 상태
-        self.state = 'Stun'
-        sx, sy = sprite_size['Stun'][0]
-        ex, ey = sprite_size['Stun'][1]
-        self.clip_x, self.clip_y, self.clip_w, self.clip_h = game_framework.carculate_image_position(self, sx, sy, ex, ey)
-        return BehaviorTree.SUCCESS
+    def stance_check(self):
+        #boy의 dir 확인
+        if common.boy.stance == -1 and self.stance == -1:
+            return BehaviorTree.SUCCESS
+        elif common.boy.stance == 1 and self.stance == 1:
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
+
+    def boy_atk_chk(self):
+        #boy가 공격 중인지 확인
+        if common.boy.atk == True:
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.FAIL
 
 
     def build_behavior_tree(self):
@@ -192,27 +176,28 @@ class Enemy:
         # 피격 판정
         # 1순위:회피 2순위:공격 도중 피격 3순위:방어 성공 4순위:피격
 
+        a_stance_dir_set = Action('Set stance and direction', self.stance_dir_set)
         a_Idle = Action('IDLE', self.Idle)
-
         a_Move = Action('MOVE', self.Move)
-
         a_Atk = Action('ATTACK', self.Attack)
         #스탠스 활용
 
         a_Def = Action('DEFEND', self.Defend)
         a_Hit = Action('HIT', self.Hit)
-        a_Stun = Action('STUN', self.stun)
+        a_Stun = Action('STUN', self.Stun)
         #boy의 dir 확인으로 방어 성공 여부 판단
         c_move_chk = Condition('move chk', self.move_chk)
         c_stance_chk = Condition('stance chk', self.stance_check)
-        c_atk_ing = Condition('atk ing', self.Atk_ing)
+        c_atk_ing_chk = Condition('atk ing chk', self.atk_ing_chk)
 
-        root = stun_chk = Sequence('Stun chk', c_atk_ing ,a_Stun)
+        root = stun_chk = Sequence('Stun chk', c_atk_ing_chk ,a_Stun)
         root = def_chk = Sequence('Def chk', c_stance_chk, a_Def)
 
-        root = Idle = Sequence('Idle', a_Atk, a_Move,a_Idle)
+        root = Idle = Sequence('Idle', a_stance_dir_set, a_Atk, a_Move,a_Idle)
         root = Hit = Selector('Hit', c_move_chk, stun_chk, def_chk, a_Hit)
+        c_boy_atk_chk = Condition('boy atk chk', self.boy_atk_chk)
+        root = Hit_chk = Sequence('Hit chk', c_boy_atk_chk, Hit)
 
-        root = Selector('Enemy Behavior', Hit, Idle)
+        root = Selector('Enemy Behavior', Hit_chk, Idle)
 
         self.bt = BehaviorTree(root)
